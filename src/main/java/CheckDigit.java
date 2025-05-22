433457f3a524c033085cdd183829a06d16a02095
AlaBuck <Albuckingham@s.sfusd.edu>
2025-05-22 13:38:20 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -8,6 +8,11 @@ public class CheckDigit
    public static int getCheck(int num) 
    {  
      /* to be implemented in part (a) */
+     int premoddeddigit = 0;
+     for(int i=0; i<=getNumberOfDigits(num); i++){
+       premoddeddigit+=(8-i)*getDigit(num, i);
+     }
+     return premoddeddigit%10
    }
  
   /** Returns true if numWithCheckDigit is valid, or false    
@@ -18,7 +23,14 @@ public class CheckDigit
    */     
    public static boolean isValid(int numWithCheckDigit)    
    {      
-     /* to be implemented in part (b) */    
+      int check = numWithCheckDigit % 10;
+      int num = numWithCheckDigit / 10;
+      int newCheck = getCheck(num);
+      if(check == newCheck) {
+        return true;
+      } else {
+        return false;
+      }
    }    
    
    /** Returns the number of digits in num. */    
